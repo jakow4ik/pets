62222faa0207a60c3beefa9543cbace99916a5f6
Nikoaly Yakovchik <jakow4ik@gmail.com>
2022-06-01 19:42:19 +0300

    - add bookmark on detail screen

--- app/src/main/java/by/dro/pets/presentation/pets_details/PetsDetailViewModel.kt
+++ app/src/main/java/by/dro/pets/presentation/pets_details/PetsDetailViewModel.kt
@@ -4,7 +4,9 @@ import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import by.dro.pets.domain.entities.Dog
+import by.dro.pets.domain.usecases.AddDogBookmarkUseCase
 import by.dro.pets.domain.usecases.GetDogsUseCase
+import by.dro.pets.domain.usecases.RemoveDogBookmarkUseCase
 import by.dro.pets.presentation.pets_list.PetsListFragment.Companion.ARG_UID
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.SharingStarted
@@ -12,11 +14,14 @@ import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.stateIn
 import javax.inject.Inject
+import kotlinx.coroutines.launch
 
 @HiltViewModel
 class PetsDetailViewModel @Inject constructor(
     getDogsUseCase: GetDogsUseCase,
-    args: SavedStateHandle
+    args: SavedStateHandle,
+    private val addDogBookmarkUseCase: AddDogBookmarkUseCase,
+    private val removeDogBookmarkUseCase: RemoveDogBookmarkUseCase,
 ) : ViewModel() {
 
     val dog: StateFlow<Dog> = getDogsUseCase
@@ -28,4 +33,14 @@ class PetsDetailViewModel @Inject constructor(
         }
         .stateIn(viewModelScope, SharingStarted.Eagerly, Dog.EMPTY)
 
+    fun onBookmarkClicked() {
+        viewModelScope.launch {
+            val pet = dog.value
+            if (pet.isBookmarked) {
+                removeDogBookmarkUseCase.execute(pet)
+            } else {
+                addDogBookmarkUseCase.execute(pet)
+            }
+        }
+    }
 }
