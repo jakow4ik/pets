d3b820ad316911c3c09a5bbc1fa1793eadc71a85
Nikoaly Yakovchik <jakow4ik@gmail.com>
2022-06-04 23:01:02 +0300

    - update about fragment

--- app/src/main/java/by/dro/pets/presentation/about/AboutFragment.kt
+++ app/src/main/java/by/dro/pets/presentation/about/AboutFragment.kt
@@ -1,12 +1,9 @@
 package by.dro.pets.presentation.about
 
 
-import android.content.Intent
-import android.net.Uri
-import android.os.Bundle
 import androidx.fragment.app.FragmentManager
 import by.dro.pets.BuildConfig
-import by.dro.pets.Config
+import by.dro.pets.R
 import by.dro.pets.databinding.FragmentAboutBinding
 import by.dro.pets.presentation.base.BaseDialogFragment
 
@@ -25,21 +22,9 @@ class AboutFragment : BaseDialogFragment<FragmentAboutBinding>(FragmentAboutBind
     }
 
     override fun initView(vb: FragmentAboutBinding) {
-        vb.versionName.text = BuildConfig.VERSION_NAME
-        vb.close.setOnClickListener { dismiss() }
-        vb.github.setOnClickListener {
-            val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse(Config.GITHUB_URL))
-            startActivity(browserIntent)
+        binding.apply {
+            aboutFragmentVersionTV.text = getString(R.string.version_n, BuildConfig.VERSION_NAME)
+            aboutFragmentCloseIV.setOnClickListener { dismiss() }
         }
-        vb.linkedin.setOnClickListener {
-            val browserIntent =
-                Intent(Intent.ACTION_VIEW, Uri.parse(Config.LINKEDIN_URL))
-            startActivity(browserIntent)
-        }
-    }
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setStyle(STYLE_NO_TITLE, 0)
     }
 }
